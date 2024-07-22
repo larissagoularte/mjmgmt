d137d1a28644d7dd248205fe5942d5940170c164
Larissa Goularte <58703854+larissagoularte@users.noreply.github.com>
2024-07-22 15:29:21 +0100

    password protect

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-import Header from "./components/Header";
-import Register from "./components/Register";
-
 function App() {
   return (
     <div className="App">
